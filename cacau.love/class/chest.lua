294260a722f4add5ed716c21abd4304c00de43e0
André Simões <alsimoes@users.noreply.github.com>
2021-02-20 18:48:40 -0300

    complete migration to classic

---
+++ cacau.love/class/chest.lua
@@ -0,0 +1,22 @@
+--! file: chest.lua
+--
+-- Scene file: Class to define the trees.
+--
+
+Chest = Object:extend()
+
+function Chest:new(x, y)
+    self.image = love.graphics.newImage("assets/dummy_chest.png")
+    self.x = x
+    self.y = y
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+    self.sx = GLOBAL.SCALE
+    self.sy = GLOBAL.SCALE
+    self.ox = self.image:getWidth() / 2
+    self.oy = self.image:getHeight() / 2
+end
+
+function Chest:draw(x, y)
+    love.graphics.draw(self.image, self.x, self.y, 0, self.sx, self.sy, self.ox)
+end
